e4ad546640c72df682f0ed94fd8c85d8572434ce
Sid0rr0 <elhungus@gmail.com>
2020-07-28 12:15:52 +0200

    job details

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Card, Badge, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 export default function Job({ job }) {
+	const [open, setOpen] = useState(false);
 	return (
-		<Card>
+		<Card className="mb-3">
 			<Card.Body>
 				<div className="d-flex justify-content-between">
 					<div>
@@ -23,8 +24,13 @@ export default function Job({ job }) {
 					<img className="d-sm-none d-md-block" height="50" src={job.company_logo} alt={job.company} />
 				</div>
 				<Card.Text>
-					<Button variant="primary">View Details</Button>
+					<Button variant="primary" onClick={() => setOpen(prevOpen => !prevOpen)}>{open ? "Hide" : "View"} Details</Button>
 				</Card.Text>
+				<Collapse in={open}>
+					<div className="mt-4">
+						<ReactMarkdown source={job.description} />
+					</div>
+				</Collapse>
 			</Card.Body>
 		</Card>
 	)
